71deda43fd7af566405c93b3e418eb893f1f5915
Tim Harris <harristimothy2186@gmail.com>
2026-06-26 16:47:41 -0600

    Add files via upload

---
+++ TimH_HelloWorldPython.py
@@ -0,0 +1,10 @@
+"""
+Developer: Tim Harris
+Date: May 8, 2026
+Purpose: Simple program to print 'Hello, World!' to the console in Python.
+"""
+
+# Output Hello, World! to the console
+print("Hello, World!")
+
+
